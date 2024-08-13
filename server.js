bc991b1d721bc4896690f228e5b0965e62fd3c48
sayedalali1 <sayedalali1@gmail.com>
2024-08-13 15:33:36 +0300

    complete

--- server.js
+++ server.js
@@ -1,19 +1,30 @@
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+
 dotenv.config();
+
+require('./config/database');
 const express = require('express');
+
+// Auth
+const verifyToken = require('./middleware/verify-token');
+
+// Controllers
+const testJWTRouter = require('./controllers/test-jwt');
+const usersRouter = require('./controllers/users');
+const profilesRouter = require('./controllers/profiles');
+
 const app = express();
-const mongoose = require('mongoose');
+const PORT = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGODB_URI);
-
-mongoose.connection.on('connected', () => {
-  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
-});
-
+app.use(morgan('dev'));
 app.use(express.json());
 
-// Routes go here
+// Routes
+app.use('/test-jwt', testJWTRouter);
+app.use('/users', usersRouter);
+app.use('/profiles', verifyToken, profilesRouter);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('The express app is ready!');
 });
